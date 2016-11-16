289deb29f5b60210c46b65639c8650b5b1b05749
wizard <rumpertumpski@gmail.com>
2016-11-16 19:42:41 -0300

    Generate image test

--- index.js
+++ index.js
@@ -2,16 +2,38 @@ var fs = require("fs");
 var Canvas = require("canvas");
 var Image = Canvas.Image;
 
+var mdc = function(a, b) {
+  if (!b) return a;
+  return mdc(b, a % b);
+};
+
 fs.readFile("./images/Source.png", function(err, squid) {
   if (err) throw err;
-  var canvas = new Canvas(16, 16);
-  var ctx = canvas.getContext("2d");
+
   var img = new Image;
   img.src = squid;
-  ctx.drawImage(img, 0, 0, img.width, img.height);
-  var stream = canvas.pngStream();
+  var tileCount = img.width / mdc(img.width, img.height);
+  var tileHeight = img.height;
+  var tileWidth = img.width / tileCount;
+
+  var tempCanvas = new Canvas(img.width, img.height);
+  var tempCtx = tempCanvas.getContext("2d");
+  tempCtx.drawImage(img, 0, 0, img.width, img.height);
+  var tempData = tempCtx.getImageData(0, 0, img.width, img.height);
+
+  var canvas = new Canvas(tileWidth * tileCount, tileHeight * tileCount);
+  var ctx = canvas.getContext("2d");
+
+  for (var i = 0; i < tileCount; i++) {
+    for (var j = 0; j < tileCount; j++) {
+      ctx.putImageData(tempData, i * tileWidth, j * tileHeight,
+        i * tileWidth, i * tileHeight, img.width, img.height);
+    }
+  }
+
   var out = fs.createWriteStream("./out.png");
-  stream
+  canvas.pngStream()
     .on("data", function(chunk) { out.write(chunk); })
-    .on("end", function() { console.log("saved..."); })
+    .on("end", function() { console.log("Generated"); });
+
 });
