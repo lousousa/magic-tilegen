f0cb32e6a4ca4b69c4b1c2faaab506af53af4e4e
luis f sousa <rumpertumpski@gmail.com>
2022-11-21 01:01:54 -0300

    add canvas

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  console.log(styles.container)
-
   return (
     <div className={styles.container}>
       <Head>
