7a1ac87351e6fae4b77c18ca63b8c9a2a4139045
luis f sousa <rumpertumpski@gmail.com>
2022-11-30 23:49:08 -0300

    reorganize images folder structure

--- components/templates/about.tsx
+++ components/templates/about.tsx
@@ -1,35 +1,71 @@
 import styled from 'styled-components'
+import Image from 'next/image'
 
 import { MainWrapper } from '../styles'
 
 export const About = () => {
   return <MainWrapper backgroundColor={'grayscale-light'}>
     <Content>
-      <h1 className="title-text">
+      <TextTitle>
         how it works
-      </h1>
+      </TextTitle>
 
-      <main className="description">
+      <DescriptionWrapper>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam molestias, dolorum voluptatum et, amet commodi, fugiat asperiores odio laborum quasi sunt repellendus dolorem voluptates ab pariatur velit cupiditate minima labore!</p>
-      </main>
+      </DescriptionWrapper>
 
-      <h1 className="title-text">
+      <TextTitle>
         base tiles
-      </h1>
+      </TextTitle>
+
+      <PreviewWrapper>
+        <div>
+          <Image
+            src="/../public/assets/images/2-full.png"
+            alt="source image"
+            fill
+          />
+        </div>
+
+        <div>
+          arrow
+        </div>
+
+        <div>
+          <Image
+            src="/../public/assets/images/2-full-tileset.png"
+            alt="source image"
+            fill
+          />
+        </div>
+      </PreviewWrapper>
     </Content>
   </MainWrapper>
 }
 
 const Content = styled.div`
-  h1 {
-    text-align: center;
-    margin-top: 96px;
-    font-size: 40px;
-  }
+  padding-bottom: 96px;
+`
 
-  .description {
-    font-size: 18px;
-    line-height: 32px;
-    margin-top: 96px;
+const TextTitle = styled.h1`
+  text-align: center;
+  margin-top: 96px;
+  font-size: 40px;
+`
+
+const DescriptionWrapper = styled.div`
+  font-size: 18px;
+  line-height: 32px;
+  margin-top: 96px;
+`
+
+const PreviewWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 96px;
+
+  div {
+    flex: 1;
+    text-align: center;
   }
 `
